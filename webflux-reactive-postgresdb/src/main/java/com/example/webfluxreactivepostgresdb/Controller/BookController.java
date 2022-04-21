8fe3184da4a2655ad1ef4f0e0806da36ebdf1c73
souravwizard20 <souravwizard20@gmail.com>
2022-04-21 18:07:11 +0530

    added response entity ti controller

--- webflux-reactive-postgresdb/src/main/java/com/example/webfluxreactivepostgresdb/Controller/BookController.java
+++ webflux-reactive-postgresdb/src/main/java/com/example/webfluxreactivepostgresdb/Controller/BookController.java
@@ -5,13 +5,16 @@ import com.example.webfluxreactivepostgresdb.Entity.BookEntity;
 import com.example.webfluxreactivepostgresdb.Repository.ReactiveBookRepository;
 import com.example.webfluxreactivepostgresdb.dto.Book;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
+
 @CrossOrigin()
 @RestController
 @RequestMapping("/books")
@@ -23,43 +26,45 @@ public class BookController {
     @Autowired
     WebClient.Builder webClientBuilder;
 
-//    CRUD Operations
-    @PostMapping("/add-book")
-    public Mono<BookEntity> addBook(@RequestBody Book book)
-    {
-        BookEntity bookEntity = new BookEntity(book.getId(),book.getTitle(),book.getAuthor());
-        return reactiveBookRepository.save(bookEntity);
+    //    CRUD Operations
+    @PostMapping("/book")
+    public Mono<ResponseEntity<BookEntity>> addBook(@RequestBody Book book) {
+        BookEntity bookEntity = new BookEntity(book.getId(), book.getTitle(), book.getAuthor());
+        return reactiveBookRepository.save(bookEntity).map(savedBook -> ResponseEntity.ok(savedBook))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
-    @GetMapping("/get-all-books")
-    public Flux<BookEntity> getAllBooks()
-    {
+    @GetMapping("/books")
+    public Flux<BookEntity> getAllBooks() {
 //        return reactiveBookRepository.findAll().delayElements(Duration.ofSeconds(3)).log();
         return reactiveBookRepository.findAll();
     }
 
-    @GetMapping(value = "/get-all-books-live", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<BookEntity> getAllLiveBooks()
-    {
+
+    @GetMapping(value = "/live/books", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<BookEntity> getAllLiveBooks() {
 //        return reactiveBookRepository.findAll().delayElements(Duration.ofSeconds(2)).log();
         return reactiveBookRepository.findAll();
     }
 
-    @GetMapping("/get-book-by-id")
-    public Mono<BookEntity> getBookById(@RequestParam (value = "id") Long id)
-    {
-        return reactiveBookRepository.findById(id);
+    @GetMapping("/book")
+    public Mono<ResponseEntity<BookEntity>> getBookById(@RequestParam(value = "id") Long id) {
+        return reactiveBookRepository.findById(id).map(book -> ResponseEntity.ok(book))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
-    @PutMapping("/update-book")
-    public Mono<BookEntity> updateBook(@RequestBody Book book){
-        BookEntity bookEntity = new BookEntity(book.getId(),book.getTitle(),book.getAuthor());
-        return reactiveBookRepository.save(bookEntity);
+    @PutMapping("/book")
+    public Mono<ResponseEntity<BookEntity>> updateBook(@RequestBody Book book) {
+        BookEntity bookEntity = new BookEntity(book.getId(), book.getTitle(), book.getAuthor());
+        return reactiveBookRepository.save(bookEntity).map(updatedBook -> ResponseEntity.ok(updatedBook))
+                .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-    @DeleteMapping("delete-book/{id}")
-    public Mono<BookEntity> deleteBook(@PathVariable("id") Long id){
-        return reactiveBookRepository.findById(id)
-                .doOnSuccess(bookEntity -> reactiveBookRepository.delete(bookEntity).subscribe());
+
+    @DeleteMapping("book/{id}")
+    public Mono<ResponseEntity<Void>> deleteBook(@PathVariable("id") Long id) {
+        return reactiveBookRepository.findById(id).flatMap(deletedBook -> reactiveBookRepository.delete(deletedBook))
+                .map(deletedBook -> ResponseEntity.ok(deletedBook))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 }
