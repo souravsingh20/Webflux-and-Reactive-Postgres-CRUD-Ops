195633f91c55f57f2a7137912b6c6cce373a0400
souravwizard20 <souravwizard20@gmail.com>
2022-04-21 11:29:57 +0530

    first commit

---
+++ webflux-reactive-postgresdb/src/main/java/com/example/webfluxreactivepostgresdb/WebfluxReactivePostgresdbApplication.java
@@ -0,0 +1,23 @@
+package com.example.webfluxreactivepostgresdb;
+
+import com.example.webfluxreactivepostgresdb.Repository.ReactiveBookRepository;
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
+
+
+@SpringBootApplication
+public class WebfluxReactivePostgresdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxReactivePostgresdbApplication.class, args);
+    }
+
+    @Bean
+    public WebClient.Builder getWebClientBuilder(){
+        return WebClient.builder();
+    }
+
+}
