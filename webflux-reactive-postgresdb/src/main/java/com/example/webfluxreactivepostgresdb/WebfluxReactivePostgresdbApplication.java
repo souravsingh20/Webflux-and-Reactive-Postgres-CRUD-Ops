2a4b1ba30095daa4bfdae5a4af4ff8204801a6f5
souravwizard20 <souravwizard20@gmail.com>
2022-04-22 14:11:38 +0530

    addes redis caching

--- webflux-reactive-postgresdb/src/main/java/com/example/webfluxreactivepostgresdb/WebfluxReactivePostgresdbApplication.java
+++ webflux-reactive-postgresdb/src/main/java/com/example/webfluxreactivepostgresdb/WebfluxReactivePostgresdbApplication.java
@@ -4,11 +4,13 @@ import com.example.webfluxreactivepostgresdb.Repository.ReactiveBookRepository;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 
 
 @SpringBootApplication
+@EnableCaching
 public class WebfluxReactivePostgresdbApplication {
 
     public static void main(String[] args) {
